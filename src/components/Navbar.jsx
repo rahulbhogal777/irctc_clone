6a2c3e4708ce59d831391ef2ec920e1dc5179671
rahulbhogal777 <rahulbhogal777@gmail.com>
2026-06-24 20:54:47 +0530

    create landing page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import styles from "../styles/Navbar.module.css";
+function Navbar() {
+  return (
+    <>
+      <nav className={StyleSheet.Navbar}>
+        {/* LOGO and name */}
+        <div>
+          
+          {/* <img src={logo} className={styles.logp} /> */}
+          
+                  <span>IRCTC Clone</span>
+        </div>
+      </nav>
+    </>
+  );
+}
